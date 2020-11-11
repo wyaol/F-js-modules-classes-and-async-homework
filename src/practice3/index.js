d36c513e847517f4c2e9d1163b4f5d32b5bfa415
wyao <289672494@qq.com>
2020-11-11 22:52:08 +0800

    [wang yao] feature: practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,20 @@
-export const getPoetry
+export const getPoetry = async () => {
+  try {
+    const response = await fetch(
+      "https://v1.jinrishici.com/all.json"
+    ).then((res) => res.json());
+    return [response.origin, response.author, response.content];
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
+
+export const getPoetry2 = () => {
+  return fetch("https://v1.jinrishici.com/all.json")
+    .then((res) => res.json())
+    .then((data) => {
+      return [data.origin, data.author, data.content];
+    })
+    .catch((e) => console.log(e));
+};
